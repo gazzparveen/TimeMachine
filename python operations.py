819d4e75c1ff840e3f4b891b434dd61efbf5bef3
gazzparveen <79242522+gazzparveen@users.noreply.github.com>
2021-02-19 23:06:10 +0530

    Add files via upload

---
+++ python operations.py
@@ -0,0 +1,12 @@
+a=50
+b=40
+c=a&b
+print("value of AND is :" ,c)
+c=a|b
+print("value of AND is :" ,c)
+c=a^b
+print("value of AND is :" ,c)
+c=a<<2
+print("value of AND is :" ,c)
+c=a>>2
+print("value of AND is :" ,c)
